9e1b7d0503855bdc2919e00a297c1b6ebd0ac672
umurik <uuldasev003@gmail.com>
2026-07-13 16:32:03 +0700

    fixing files with black, isort and etc

--- src/classes.py
+++ src/classes.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+
 class Product:
     """Класс для продуктов.
 
@@ -11,6 +12,7 @@ class Product:
         price (float): Цена продукта.
         quantity (int): Количество продукта на складе.
     """
+
     name: str
     description: str
     __price: float
@@ -47,33 +49,40 @@ class Product:
     def price(self, price):
         """Устанавливает новую цену продукта.
 
-        Если новая цена ниже текущей, запрашивает подтверждение пользователя в консоли.
+        Если новая цена ниже текущей,
+         запрашивает подтверждение пользователя в консоли.
         Не позволяет установить нулевую или отрицательную цену.
 
         Args:
             price (float): Новая цена продукта.
         """
         if price <= 0:
-            print('Цена не должна быть нулевой или отрицательной')
+            print("Цена не должна быть нулевой или отрицательной")
             return
         if price < self.__price:
             while True:
-                print('Вы устанавливаете цену ниже, чем была')
-                user_input = str(input('Продолжить?(Y/n)')).upper()
-                if user_input == 'N':
+                print("Вы устанавливаете цену ниже, чем была")
+                user_input = str(input("Продолжить?(Y/n)")).upper()
+                if user_input == "N":
                     return
-                if user_input in ('Y', ''):
+                if user_input in ("Y", ""):
                     break
         self.__price = price
+
     @classmethod
-    def new_product(cls, product_data: dict, products_list: list[Product] | None = None):
+    def new_product(
+        cls, product_data: dict, products_list: list[Product] | None = None
+    ):
         """Добавляет новый продукт.
 
-        Создает новый экземпляр класса в случае, если product_list пуст, иначе обновляет информацию о цене, описании и количестве.
+        Создает новый экземпляр класса в случае, если product_list пуст,
+        иначе обновляет информацию о цене, описании и количестве.
 
         Args:
-            product_data (dict): Словарь с полями 'name', 'description', 'price', 'quantity'
-            products_list (list[Product]): Список с существующими экземплярами класса.
+            product_data (dict): Словарь с полями 'name',
+            'description', 'price', 'quantity'
+            products_list (list[Product]):
+            Список с существующими экземплярами класса.
 
         Returns:
             Product: Экземпляр продукта.
@@ -82,12 +91,12 @@ class Product:
             KeyError: Если Name отсутствует.
             ValueError: Если price или quantity меньше 0.
         """
-        if 'name' not in product_data:
+        if "name" not in product_data:
             raise KeyError("Ключ name отсутствует в словаре")
-        name = product_data.get('name')
-        description = product_data.get('description', '')
-        price = product_data.get('price', 0)
-        quantity = product_data.get('quantity', 0)
+        name = product_data.get("name")
+        description = product_data.get("description", "")
+        price = product_data.get("price", 0)
+        quantity = product_data.get("quantity", 0)
         if price < 0:
             raise ValueError("Цена не может быть отрицательной")
         if quantity < 0:
@@ -101,11 +110,10 @@ class Product:
         return cls(name, description, price, quantity)
 
 
-
 class Category:
     name: str
     description: str
-    __products: list = None
+    __products: list | None = None
     category_count = 0
     product_count = 0
 
@@ -115,11 +123,16 @@ class Category:
         self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+
     @property
     def products(self):
-        return ''.join(f'{product.name}, '
-                       f'{product.price} руб. '
-                       f'Остаток: {product.quantity} шт.\n' for product in self.__products)
+        return "".join(
+            f"{product.name}, "
+            f"{product.price} руб. "
+            f"Остаток: {product.quantity} шт.\n"
+            for product in self.__products
+        )
+
     def add_product(self, product):
         Category.product_count += 1
         self.__products.append(product)
