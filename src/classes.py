720badebc1320a43219648cc2f529e40c537627d
umurik <uuldasev003@gmail.com>
2026-07-20 15:02:30 +0700

    adding Inheritance classes

--- src/classes.py
+++ src/classes.py
@@ -11,12 +11,14 @@ class Product:
         description (str): Описание продукта.
         price (float): Цена продукта.
         quantity (int): Количество продукта на складе.
+        color (str): Цвет продукта(необязателен).
     """
 
     name: str
     description: str
     __price: float
     quantity: int
+    color: str | None
 
     def __init__(
         self,
@@ -24,6 +26,7 @@ class Product:
         description: str,
         price: float,
         quantity: int,
+        color: str | None = None,
     ):
         """Инициализация продукта
 
@@ -32,19 +35,21 @@ class Product:
             description (str): Описание продукта.
             price (float): Цена продукта.
             quantity (int): Количество продукта на складе.
+            color (str | None): Цвет продукта.
         """
 
         self.name = name
         self.description = description
-
         self.__price = price
         self.quantity = quantity
+        self.color = color
 
     def __str__(self):
-        return (f'{self.name}, {self.price} руб. '
-                f'Остаток: {self.quantity} шт.\n')
+        return f"{self.name}, {self.price} руб. " f"Остаток: {self.quantity} шт.\n"
 
     def __add__(self, addend):
+        if type(self) is not type(addend):
+            raise TypeError("Невозможно сложить разные продукты!")
         total = (self.price * self.quantity) + (addend.price * addend.quantity)
         if total % 1 == 0:
             return int(total)
@@ -120,6 +125,32 @@ class Product:
         return cls(name, description, price, quantity)
 
 
+class Smartphone(Product):
+    efficiency: float
+    model: str
+    memory: int
+
+    def __init__(
+        self, name, description, price, quantity, efficiency, model, memory, color
+    ):
+        super().__init__(name, description, price, quantity, color)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+
+
+class LawnGrass(Product):
+    country: str
+    germination_period: str
+
+    def __init__(
+        self, name, description, price, quantity, country, germination_period, color
+    ):
+        super().__init__(name, description, price, quantity, color)
+        self.country = country
+        self.germination_period = germination_period
+
+
 class Category:
     name: str
     description: str
@@ -138,14 +169,14 @@ class Category:
         total = 0
         for product in self.__products:
             total += product.quantity
-        return f'{self.name}, количество продуктов: {total} шт.'
+        return f"{self.name}, количество продуктов: {total} шт."
 
     @property
     def products(self):
-        return "".join(
-            str(product) for product in self.__products
-        )
+        return "".join(str(product) for product in self.__products)
 
     def add_product(self, product):
+        if not isinstance(product, Product):
+            raise TypeError("Объект не является классом Product")
         Category.product_count += 1
         self.__products.append(product)
